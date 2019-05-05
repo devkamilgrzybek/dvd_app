2389b3ac27309803e9e3dca595ecc5403918a832
Kamil Grzybek <dev.grzybek.kamil@gmail.com>
2019-05-05 19:51:55 +0200

    initial

---
+++ dvdRental/src/main/java/pl/dvdrental/DvdrentalApplication.java
@@ -0,0 +1,16 @@
+package pl.dvdrental;
+
+import java.io.IOException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdrentalApplication {
+
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(DvdrentalApplication.class, args);
+	}
+
+}
+
